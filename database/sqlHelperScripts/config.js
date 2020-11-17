523573b5d5fcb0dcb4fc09b05a97dee8fcb3b080
Liam Murray <murrayl9797@gmail.com>
2020-11-16 19:49:07 -0500

    Delete config.js

--- database/sqlHelperScripts/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  DB_USER: 'postgres',
-  DB_HOST: '100.25.168.124',
-  DB_DATABASE: 'postgres',
-  DB_PASSWORD: 'password',
-  DB_PORT: 5432,
-  CSV_PATH: '/Users/liammurray/Desktop/hr/sdc/sdc-csvs',
-}
